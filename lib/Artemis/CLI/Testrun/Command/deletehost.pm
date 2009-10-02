7f39a47adecf260b9734e8c2076e00dc2c4cd23f
Maik Hentsche <maik.hentsche@amd.com>
2009-10-02 13:59:21 +0200

    CLI: added module to delete hosts

---
+++ lib/Artemis/CLI/Testrun/Command/deletehost.pm
@@ -0,0 +1,68 @@
+package Artemis::CLI::Testrun::Command::deletehost;
+
+use 5.010;
+use strict;
+use warnings;
+
+use parent 'App::Cmd::Command';
+
+use Data::Dumper;
+use Artemis::Model 'model';
+use Artemis::Schema::TestrunDB;
+use Artemis::Cmd::Testrun;
+
+sub abstract {
+        'Delete a host'
+}
+
+sub opt_spec {
+        return (
+                [ "verbose|v",  "some more informational output" ],
+                [ "really",   "really execute the command"     ],
+                [ "id=s@",    "delete particular host",    ],
+               );
+}
+
+sub usage_desc {
+        my $allowed_opts = join ' | ', map { '--'.$_ } _allowed_opts();
+        "artemis-testruns deletehost [ " . $allowed_opts ." ]";
+}
+
+sub _allowed_opts {
+        my @allowed_opts = map { $_->[0] } opt_spec();
+}
+
+sub _extract_bare_option_names {
+        map { s/=.*//; $_} _allowed_opts();
+}
+
+sub validate_args {
+        my ($self, $opt, $args) = @_;
+
+
+        my $msg = "Unknown option";
+        $msg   .= ($args and $#{$args} >=1) ? 's' : '';
+        $msg   .= ": ";
+        say STDERR $msg, join(', ',@$args) if ($args and @$args);
+
+        my $allowed_opts_re = join '|', _extract_bare_option_names();
+
+        return 1 if grep /$allowed_opts_re/, keys %$opt;
+        die $self->usage->text;
+}
+
+sub run {
+        my ($self, $opt, $args) = @_;
+ ID:
+        foreach my $id (@{$opt->{id}}){
+                my $host = model('TestrunDB')->resultset('Host')->find($id);
+                next ID if not $host;
+                my $name = $host->name;
+                $host->delete();
+                say "Deleted host $name with id $id" if $opt->{verbose};
+        }
+}
+
+1;
+
+# perl -Ilib bin/artemis-testrun delete --id 16
