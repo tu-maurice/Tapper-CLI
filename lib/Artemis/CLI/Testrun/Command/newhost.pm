b49df05fd57b8caa1a62f4bea7afb13dec4792ab
Maik Hentsche <maik.hentsche@amd.com>
2009-10-02 14:09:11 +0200

    CLI: added queue handling to newhost module

--- lib/Artemis/CLI/Testrun/Command/newhost.pm
+++ lib/Artemis/CLI/Testrun/Command/newhost.pm
@@ -20,6 +20,7 @@ my $options =  {
                 "verbose"          => { text => "some more informational output" },
                 "name"             => { text => "TEXT; name",    type => 'string' },
                 "active"           => { text => "INT; MCP can use this host, default 0", type => 'bool' },
+                "queue"            => { text => "TEXT; Bind host to named queue (queue has to exists already)", type => 'manystring'}
                 };
 
 sub opt_spec {
@@ -42,7 +43,7 @@ sub opt_spec {
 
 sub usage_desc
 {
-        "artemis-testruns newqueue --name=s --active=s [ --verbose ]";
+        "artemis-testruns newqueue --name=s [ --queue=s@ --active=s --verbose ]*";
 }
 
 sub validate_args
@@ -50,9 +51,19 @@ sub validate_args
         my ($self, $opt, $args) = @_;
 
         say "Missing argument --name"     unless  $opt->{name};
+        if ($opt->{queue}) {
+                foreach my $queue(@{$opt->{queue}}) {
+                        my $queue_rs = model('TestrunDB')->resultset('Queue')->search({name => $queue});
+                        if (not $queue_rs->count) {
+                                say STDERR "No such queue: $queue";
+                                my @queue_names = map {$_->name} model('TestrunDB')->resultset('Queue')->all;
+                                say STDERR "Existing queues: ",join ", ",@queue_names;
+                                die $self->usage->text;
+                        }
+                }
+        }
 
         return 1 if $opt->{name};
-
         die $self->usage->text;
 }
 
@@ -64,11 +75,28 @@ sub new_host
         my $host = {
                     name   => $opt->{name},
                     active => $opt->{active},
+                    free   => 1,
                    };
-
+        
         my $newhost = model('TestrunDB')->resultset('Host')->new($host);
         $newhost->insert();
         die "Can't create new host" if not $newhost;
+
+        if ($opt->{queue}) {
+                foreach my $queue (@{$opt->{queue}}) {
+                        my $queue_rs   = model('TestrunDB')->resultset('Queue')->search({name => $queue});
+                        if (not $queue_rs->count) {
+                                $newhost->delete();
+                                say STDERR qq(Did not find queue "$queue");
+                        }
+                        my $queue_host = model('TestrunDB')->resultset('QueueHost')->new({
+                                                                                          host_id  => $newhost->id,
+                                                                                          queue_id => $queue_rs->first->id,
+                                                                                         });
+                        $queue_host->insert();
+                }
+        }
+
         say $newhost->id;
 }
 
