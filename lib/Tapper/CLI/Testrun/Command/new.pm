c4261a474b92bb9d6206bf00bf25811d33631b93
Steffen Schwigon <steffen.schwigon@amd.com>
2011-02-08 15:42:23 +0100

    rename - src/Tapper-CLI (basedir)

---
+++ lib/Tapper/CLI/Testrun/Command/new.pm
@@ -0,0 +1,294 @@
+package Tapper::CLI::Testrun::Command::new;
+
+use 5.010;
+
+use strict;
+use warnings;
+
+use parent 'App::Cmd::Command';
+
+use Tapper::Cmd::Requested;
+
+use DateTime::Format::Natural;
+use File::Slurp 'slurp';
+use Template;
+
+use Tapper::Cmd::Precondition;
+use Tapper::Cmd::Testrun;
+use Tapper::Model 'model';
+
+
+no warnings 'uninitialized';
+
+
+sub abstract {
+        'Update an existing testrun'
+}
+
+
+my $options = { "verbose"           => { text => "some more informational output" },
+                "notes"             => { text => "TEXT; notes", type => 'string' },
+                "shortname"         => { text => "TEXT; shortname", type => 'string' },
+                "queue"             => { text => "STRING, default=AdHoc", type => 'string' },
+                "topic"             => { text => "STRING, default=Misc; one of: Kernel, Xen, KVM, Hardware, Distribution, Benchmark, Software, Misc", type => 'string' },
+                "owner"             => { text => "STRING, default=\$USER; user login name", type => 'string' },
+                "wait_after_tests"  => { text => "BOOL, default=0; wait after testrun for human investigation", type => 'bool' },
+                "auto_rerun"        => { text => "BOOL, default=0; put this testrun into db again when it is chosen by scheduler", type => 'bool' },
+                "earliest"          => { text => "STRING, default=now; don't start testrun before this time (format: YYYY-MM-DD hh:mm:ss or now)", type => 'string' },
+                "precondition"      => { text => "assigned precondition ids", needed => 1, type => 'manystring'  },
+                "macroprecond"      => { text => "STRING, use this macro precondition file", needed => 1 , type => 'string' },
+                "D"                 => { text => "Define a key=value pair used in macro preconditions", type => 'keyvalue' },
+                "rerun_on_error"    => { text => "INT, retry this testrun this many times if an error occurs", type => 'string' },
+                "requested_host"    => { text => "String; name one possible host for this testrequest; \n\t\t\t\t  ".
+                                                "multiple requested hosts are OR evaluated, i.e. each is appropriate", type => 'manystring' },
+                "requested_feature" => { text => "String; description of one requested feature of a matching host for this testrequest; \n\t\t\t\t  ".
+                                                "multiple requested features are AND evaluated, i.e. each must fit; ".
+                                                "not evaluated if a matching requested host is found already", type => 'manystring' },
+                "priority"          => { text => "Boolean; This is a very important testrun that should bypass scheduling and not wait for others", type => 'withno' },
+
+                };
+
+sub opt_spec {
+        my @opt_spec;
+        foreach my $key (keys %$options) {
+                my $pushkey = $key;
+                $pushkey    = $pushkey."|".$options->{$key}->{short} if $options->{$key}->{short};
+
+                given($options->{$key}->{type}){
+                        when ("string")        {$pushkey .="=s";}
+                        when ("withno")        {$pushkey .="!";}
+                        when ("manystring")    {$pushkey .="=s@";}
+                        when ("optmanystring") {$pushkey .=":s@";}
+                        when ("keyvalue")      {$pushkey .="=s%";}
+                }
+                push @opt_spec, [$pushkey, $options->{$key}->{text}];
+        }
+        return (
+                @opt_spec
+               );
+}
+
+
+sub usage_desc
+{
+        my $allowed_opts = join ' ', map { '--'.$_ } _allowed_opts();
+        "tapper-testrun new  [ --requested_host=s@ | --requested_feature=s@ | --topic=s | --queue=s | --notes=s | --shortname=s | --owner=s | --wait_after_tests=s | --macroprecond=s | -Dkey=val | --auto_rerun]*";
+}
+
+sub _allowed_opts
+{
+        my @allowed_opts = map { $_->[0] } opt_spec();
+}
+
+sub convert_format_datetime_natural
+{
+        my ($self, $opt, $args) = @_;
+        # handle natural datetimes
+        if ($opt->{earliest}) {
+                my $parser = DateTime::Format::Natural->new;
+                my $dt = $parser->parse_datetime($opt->{earliest});
+                if ($parser->success) {
+                        print("%02d.%02d.%4d %02d:%02d:%02d\n", $dt->day,
+                              $dt->month,
+                              $dt->year,
+                              $dt->hour,
+                              $dt->min,
+                              $dt->sec) if $opt->{verbose};
+                        $opt->{earliest} = $dt;
+                } else {
+                        die $parser->error;
+                }
+        }
+}
+
+sub validate_args
+{
+        my ($self, $opt, $args) = @_;
+
+
+        # -- topic constraints --
+        my $topic    = $opt->{topic} || '';
+
+        my $msg = "Unknown option";
+        $msg   .= ($args and $#{$args} >=1) ? 's' : '';
+        $msg   .= ": ";
+        if (($args and @$args)) {
+                say STDERR $msg, join(', ',@$args);
+                die $self->usage->text;
+        }
+
+
+        my @needed_opts;
+        my $precondition_ok;
+        foreach my $key (keys %$options) {
+                push @needed_opts, $key if  $options->{$key}->{needed};
+        }
+
+        my $needed_opts_re = join '|', @needed_opts;
+
+        if (grep /$needed_opts_re/, keys %$opt) {
+                $precondition_ok = 1;
+        } else {
+                say STDERR "At least one of ",join ", ",@needed_opts," is required.";
+        }
+
+
+        $self->convert_format_datetime_natural;
+
+        my $macrovalues_ok = 1;
+        if ($opt->{macroprecond}) {
+                my @precond_lines =  slurp $opt->{macroprecond};
+                my @mandatory;
+                my $required = '';
+                foreach my $line (@precond_lines) {
+                        ($required) = $line =~/# (?:tapper[_-])?mandatory[_-]fields:\s*(.+)/;
+                        last if $required;
+                }
+
+                my $delim = qr/,+\s*/;
+                foreach my $field (split $delim, $required) {
+                        $field =~ s/\s+//g;
+                        my ($name, $type) = split /\./, $field;
+                        if (not $opt->{d}{$name}) {
+                                say STDERR "Expected macro field '$name' missing.";
+                                $macrovalues_ok = 0;
+                        }
+                }
+                $opt->{macropreconds} = join '',@precond_lines;
+        }
+
+        if (exists $opt->{rerun_on_error} and not int($opt->{rerun_on_error})) {
+                say STDERR "Value for rerun_on_error ($opt->{rerun_on_error}) can not be parsed as integer value. Won't set rerun_on_error";
+        }
+
+        return 1 if $precondition_ok and $macrovalues_ok;
+
+        die $self->usage->text;
+}
+
+sub execute
+{
+        my ($self, $opt, $args) = @_;
+
+        $self->new_runtest ($opt, $args);
+}
+
+=head2 create_macro_preconditions
+
+Process a macroprecondition. This includes substitions using
+Template::Toolkit, separating the individual preconditions that are part of
+the macroprecondition and putting them into the database. Parameters fit the
+App::Cmd::Command API.
+
+@param hashref - hash containing options
+@param hashref - hash containing arguments
+
+@returnlist array containing precondition ids
+
+=cut
+
+sub create_macro_preconditions
+{
+        my ($self, $opt, $args) = @_;
+
+        my $D             = $opt->{d}; # options are auto-down-cased
+        my $tt            = new Template ();
+        my $macro         = $opt->{macropreconds};
+        my $ttapplied;
+
+        $tt->process(\$macro, $D, \$ttapplied) || die $tt->error();
+
+        my $precondition = Tapper::Cmd::Precondition->new();
+        my @ids = $precondition->add($ttapplied);
+        return @ids;
+}
+
+
+sub add_host
+{
+        my ($self, $testrun_id, $host) = @_;
+        my $cmd =  Tapper::Cmd::Requested->new();
+        my $id = $cmd->add_host($testrun_id, $host);
+        return $id;
+
+}
+
+
+sub add_feature
+{
+        my ($self, $testrun_id, $feature) = @_;
+        my $cmd = Tapper::Cmd::Requested->new();
+        my $id = $cmd->add_feature($testrun_id, $feature);
+        return $id;
+
+}
+
+sub analyse_preconditions
+{
+        my ($self, @ids) = @_;
+}
+
+
+sub new_runtest
+{
+        my ($self, $opt, $args) = @_;
+
+
+        my $testrun = {
+                       auto_rerun     => $opt->{auto_rerun},
+                       date           => $opt->{earliest}            || DateTime->now,
+                       notes          => $opt->{notes}               || '',
+                       owner          => $opt->{owner}               || $ENV{USER},
+                       priority       => $opt->{priority},
+                       queue          => $opt->{queue}               || 'AdHoc',
+                       rerun_on_error => int($opt->{rerun_on_error}) || 0,
+                       shortname      => $opt->{shortname}           || '',
+                       topic          => $opt->{topic}               || 'Misc',
+                      };
+        my @ids;
+
+        @ids = $self->create_macro_preconditions($opt, $args) if $opt->{macroprecond};
+        push @ids, @{$opt->{precondition}} if $opt->{precondition};
+
+        die "No valid preconditions given" if not @ids;
+
+        my $cmd = Tapper::Cmd::Testrun->new();
+        my $testrun_id = $cmd->add($testrun);
+        die "Can't create new testrun because of an unknown error" if not $testrun_id;
+
+        my $testrun_search = model('TestrunDB')->resultset('Testrun')->find($testrun_id);
+
+        my $retval = $self->analyse_preconditions(@ids);
+
+        $retval = $cmd->assign_preconditions($testrun_id, @ids);
+        if ($retval) {
+                $testrun_search->delete();
+                die $retval;
+        }
+
+        if ($opt->{requested_host}) {
+                foreach my $host (@{$opt->{requested_host}}) {
+                        push @ids, $self->add_host($testrun_id, $host);
+                }
+        }
+
+        if ($opt->{requested_feature}) {
+                foreach my $feature (@{$opt->{requested_feature}}) {
+                        push @ids, $self->add_feature($testrun_id, $feature);
+                }
+        }
+        $testrun_search->testrun_scheduling->status('schedule');
+        $testrun_search->testrun_scheduling->update;
+
+        if ($opt->{verbose}) {
+                say $testrun_search->to_string;
+        } else {
+                say $testrun_id;
+        }
+}
+
+
+
+# perl -Ilib bin/tapper-testrun new --topic=Software --precondition=14  --owner=ss5
+
+1;
---
+++ t/tapper-cli-testrun.t
@@ -0,0 +1,175 @@
+#! /usr/bin/env perl
+
+use strict;
+use warnings;
+
+use Test::More;
+use Tapper::CLI::Testrun;
+use Tapper::CLI::Testrun::Command::list;
+use Tapper::Schema::TestTools;
+use Tapper::Model 'model';
+use Test::Fixture::DBIC::Schema;
+
+# -----------------------------------------------------------------------------------------------------------------
+construct_fixture( schema  => testrundb_schema, fixture => 't/fixtures/testrundb/testruns_with_scheduling.yml' );
+# -----------------------------------------------------------------------------------------------------------------
+
+my $testrun = Tapper::CLI::Testrun::Command::list::_get_entry_by_id (23); # perfmon
+
+is($testrun->id, 23, "testrun id");
+is($testrun->notes, 'perfmon', "testrun notes");
+is($testrun->shortname, 'perfmon', "testrun shortname");
+is($testrun->topic_name, 'Software', "testrun topic_name");
+
+
+
+my $precond_id = `/usr/bin/env perl -Ilib bin/tapper-testrun newprecondition  --condition="precondition_type: image\nname: suse.tgz"`;
+chomp $precond_id;
+
+my $precond = model('TestrunDB')->resultset('Precondition')->find($precond_id);
+ok($precond->id, 'inserted precond / id');
+like($precond->precondition, qr"precondition_type: image", 'inserted precond / yaml');
+
+# --------------------------------------------------
+
+my $old_precond_id = $precond_id;
+$precond_id = `/usr/bin/env perl -Ilib bin/tapper-testrun updateprecondition --id=$old_precond_id --shortname="foobar-perl-5.11" --condition="precondition_type: file\nname: some_file"`;
+chomp $precond_id;
+
+$precond = model('TestrunDB')->resultset('Precondition')->find($precond_id);
+is($precond->id, $old_precond_id, 'update precond / id');
+is($precond->shortname, 'foobar-perl-5.11', 'update precond / shortname');
+like($precond->precondition, qr'precondition_type: file', 'update precond / yaml');
+
+# --------------------------------------------------
+
+my $testrun_id = `/usr/bin/env perl -Ilib bin/tapper-testrun new --topic=Software --requested_host=iring --precondition=1`;
+chomp $testrun_id;
+
+$testrun = model('TestrunDB')->resultset('Testrun')->find($testrun_id);
+ok($testrun->id, 'inserted testrun / id');
+is($testrun->testrun_scheduling->requested_hosts->first->host->name, 'iring', 'inserted testrun / first requested host');
+is($testrun->topic_name, 'Software', 'Topic for new testrun');
+
+# --------------------------------------------------
+#
+# Testrun with requested feature
+#
+
+$testrun_id = `/usr/bin/env perl -Ilib bin/tapper-testrun new --requested_feature='mem > 4096' --queue=KVM --precondition=1`;
+chomp $testrun_id;
+
+$testrun = model('TestrunDB')->resultset('Testrun')->find($testrun_id);
+ok($testrun->id, 'inserted testrun / id');
+is($testrun->testrun_scheduling->requested_features->first->feature, 'mem > 4096', 'inserted testrun / first requested feature');
+is($testrun->testrun_scheduling->queue->name, 'KVM', 'inserted testrun / Queue');
+
+
+
+# --------------------------------------------------
+SKIP: {
+        skip "Update is currently deprecated", 3;
+        my $old_testrun_id = $testrun_id;
+        $testrun_id = `/usr/bin/env perl -Ilib bin/tapper-testrun update --id=$old_testrun_id --topic=Hardware --requested_host=iring`;
+        chomp $testrun_id;
+
+        $testrun = model('TestrunDB')->resultset('Testrun')->find($testrun_id);
+        is($testrun->id, $old_testrun_id, 'updated testrun / id');
+        is($testrun->topic_name, "Hardware", 'updated testrun / topic');
+        is($testrun->testrun_scheduling->requested_hosts->first->host->name, 'iring', 'updated testrun / first requested host');
+}
+
+# --------------------------------------------------
+
+`/usr/bin/env perl -Ilib bin/tapper-testrun delete --id=$testrun_id --really`;
+$testrun = model('TestrunDB')->resultset('Testrun')->find($testrun_id);
+is($testrun, undef, "delete testrun");
+
+`/usr/bin/env perl -Ilib bin/tapper-testrun deleteprecondition --id=$precond_id --really`;
+$precond = model('TestrunDB')->resultset('Precondition')->find($precond_id);
+is($precond, undef, "delete precond");
+
+# --------------------------------------------------
+
+$testrun_id = `/usr/bin/env perl -Ilib bin/tapper-testrun new --macroprecond=t/files/kernel_boot.mpc -Dkernel_version=2.6.19 --requested_host=iring`;
+chomp $testrun_id;
+$testrun = model('TestrunDB')->resultset('Testrun')->search({id => $testrun_id,})->first();
+
+my @precond_array = $testrun->ordered_preconditions;
+
+is($precond_array[0]->precondition_as_hash->{precondition_type}, "package",'Parsing macropreconditions, first sub precondition');
+is($precond_array[1]->precondition_as_hash->{precondition_type}, "exec",'Parsing macropreconditions, second sub precondition');
+is($precond_array[1]->precondition_as_hash->{options}->[0], "2.6.19",'Parsing macropreconditions, template toolkit substitution');
+is($precond_array[0]->precondition_as_hash->{filename}, "kernel/linux-2.6.19.tar.gz",'Parsing macropreconditions, template toolkit with if block');
+
+$testrun_id = `/usr/bin/env perl -Ilib bin/tapper-testrun new --macroprecond=t/files/kernel_boot.mpc --requested_host=iring 2>&1`;
+chomp $testrun_id;
+like($testrun_id, qr/Expected macro field 'kernel_version' missing./, "missing mandatory field recognized");
+
+$testrun_id = `/usr/bin/env perl -Ilib bin/tapper-testrun new --requested_host=iring 2>&1`;
+chomp $testrun_id;
+like($testrun_id, qr/At least one of .+ is required./, "Prevented testrun without precondition");
+
+$testrun_id = `/usr/bin/env perl -Ilib bin/tapper-testrun rerun --testrun=23`;
+chomp $testrun_id;
+ok($testrun_id, 'Got some testrun');
+isnt($testrun_id, 23, 'Rerun creates new testrun');
+$testrun = model('TestrunDB')->resultset('Testrun')->find($testrun_id);
+my $testrun_old = model('TestrunDB')->resultset('Testrun')->find(23);
+@precond_array = $testrun->ordered_preconditions;
+my @precond_array_old = $testrun_old->ordered_preconditions;
+is_deeply(\@precond_array, \@precond_array_old, 'Rerun testrun with same preconditions');
+
+# --------------------------------------------------
+
+my $queue_id = `/usr/bin/env perl -Ilib bin/tapper-testrun newqueue  --name="Affe" --priority=4711`;
+chomp $queue_id;
+
+my $queue = model('TestrunDB')->resultset('Queue')->find($queue_id);
+ok($queue->id, 'inserted queue / id');
+is($queue->name, "Affe", 'inserted queue / name');
+is($queue->priority, 4711, 'inserted queue / priority');
+
+$testrun_id = `/usr/bin/env perl -Ilib bin/tapper-testrun new --topic=Software --requested_host=iring --precondition=1 --precondition=2 --queue=Affe --auto_rerun`;
+chomp $testrun_id;
+
+$testrun = model('TestrunDB')->resultset('Testrun')->find($testrun_id);
+ok($testrun->id, 'inserted testrun / id');
+is($testrun->topic_name, 'Software', 'Topic for new testrun');
+is($testrun->testrun_scheduling->queue->name, 'Affe', 'Queue for new testrun');
+is($testrun->testrun_scheduling->auto_rerun, '1', 'Auto_rerun new testrun');
+
+# --------------------------------------------------
+
+my $host_id = `/usr/bin/env perl -Ilib bin/tapper-testrun newhost --name=fritz --active`;
+chomp $host_id;
+
+my $host = model('TestrunDB')->resultset('Host')->find($host_id);
+ok($host->id, 'inserted testrun has id');
+is($host->id, $host_id, 'inserted testrun has right id');
+is($host->name, 'fritz', 'Name of new host');
+
+
+# --------------------------------------------------
+
+$testrun_id = `/usr/bin/env perl -Ilib bin/tapper-testrun new --topic=Software --rerun_on_error=3 --precondition=1`;
+chomp $testrun_id;
+
+$testrun = model('TestrunDB')->resultset('Testrun')->find($testrun_id);
+ok($testrun->id, 'inserted testrun / id');
+is($testrun->rerun_on_error, 3, 'Setting rerun on error');
+
+
+# --------------------------------------------------
+#         Priorities
+# --------------------------------------------------
+$testrun_id = `/usr/bin/env perl -Ilib bin/tapper-testrun new --topic=Software --priority --precondition=1`;
+chomp $testrun_id;
+$testrun = model('TestrunDB')->resultset('Testrun')->find($testrun_id);
+ok($testrun->id, 'inserted testrun / id');
+ok(defined($testrun->testrun_scheduling->prioqueue_seq), 'inserted testrun is in priority queue');
+
+
+
+
+done_testing();
