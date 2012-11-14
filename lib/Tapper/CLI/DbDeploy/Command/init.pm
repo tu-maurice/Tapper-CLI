6b85b0507ca224c4301c801937975b8b3f083378
Steffen Schwigon <ss5@renormalist.net>
2012-11-14 11:24:29 +0100

    [RFK] move code to Tapper::Cmd::* backend
    This way we can re-use it independently from
    the commandline tools, in particular here to
    initialize/deploy databases as part of another
    CLI tool: 'tapper init'.

--- lib/Tapper/CLI/DbDeploy/Command/init.pm
+++ lib/Tapper/CLI/DbDeploy/Command/init.pm
@@ -8,6 +8,7 @@ use warnings;
 use parent 'App::Cmd::Command';
 
 use Tapper::CLI::DbDeploy;
+use Tapper::Cmd::DbDeploy;
 use Data::Dumper;
 use Tapper::Schema::TestrunDB;
 use Tapper::Schema::ReportsDB;
@@ -58,69 +59,12 @@ sub validate_args {
         die $self->usage->text;
 }
 
-sub insert_initial_values
-{
-        my ($schema, $db) = @_;
-
-        if ($db eq 'TestrunDB')
-        {
-                # ---------- Topic ----------
-
-                # official topics
-                my %topic_description = %Tapper::Schema::TestrunDB::Result::Topic::topic_description;
-
-                foreach my $topic_name(keys %topic_description) {
-                        my $topic = $schema->resultset('Topic')->new
-                            ({ name        => $topic_name,
-                               description => $topic_description{$topic_name},
-                             });
-                        $topic->insert;
-                }
-                my $queue = $schema->resultset('Queue')->new
-                  ({ name     => 'AdHoc',
-                     priority => 1000,
-                     active   => 1,
-                   });
-                $queue->insert;
-
-        }
-}
-
-sub init_db
-{
-        my ($self, $db) = @_;
-
-        local $| =1;
-
-        my $dsn  = Tapper::Config->subconfig->{database}{$db}{dsn};
-        my $user = Tapper::Config->subconfig->{database}{$db}{username};
-        my $pw   = Tapper::Config->subconfig->{database}{$db}{password};
-
-        # ----- really? -----
-        print "dsn = $dsn\n";
-        print "Really delete all existing content and initialize from scratch (y/N)? ";
-        read STDIN, my $answer, 1;
-        do { print "Quit.\n"; return } unless lc $answer eq 'y';
-
-        # ----- delete sqlite file -----
-        if ($dsn =~ /dbi:SQLite:dbname/) {
-                my ($tmpfname) = $dsn =~ m,dbi:SQLite:dbname=([\w./]+),i;
-                unlink $tmpfname;
-        }
-
-        my $schema;
-        $schema = Tapper::Schema::TestrunDB->connect ($dsn, $user, $pw) if $db eq 'TestrunDB';
-        $schema = Tapper::Schema::ReportsDB->connect ($dsn, $user, $pw) if $db eq 'ReportsDB';
-        $schema->deploy({ add_drop_table => 1 }); # may fail, does not provide correct order to drop tables
-        insert_initial_values($schema, $db);
-}
-
 sub run
 {
         my ($self, $opt, $args) = @_;
 
-        my $db  = $opt->{db};
-        $self->init_db($db);
+        my $cmd = Tapper::Cmd::DbDeploy->new;
+        $cmd->dbdeploy($opt->{db});
 }
 
 
