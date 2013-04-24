cd5d1d8a58557ab18434443ee0b3e1ba2a37d1a2
Maik Hentsche <caldrin@amazon.com>
2013-04-24 06:06:33 -0700

    deprecate tapper-testrun newscenario

--- lib/Tapper/CLI/Testrun/Command/newscenario.pm
+++ lib/Tapper/CLI/Testrun/Command/newscenario.pm
@@ -16,192 +16,20 @@ use Tapper::Cmd::Requested;
 use Tapper::Config;
 
 sub abstract {
-        'Create a new scenario';
+        'Deprecated, use tapper scenario-new';
 }
 
-
-my $options = { "verbose" => { text => "some more informational output" },
-                "quiet"   => { text => "only show scenario id, not testrun ids"},
-                "D"       => { text => "Define a key=value pair used in macro preconditions", type => 'keyvalue' },
-                "file"    => { text => "String; use macro scenario file", type => 'string' },
-                };
-
-sub opt_spec {
-        my @opt_spec;
-        foreach my $key (keys %$options) {
-                my $pushkey;
-                given($options->{$key}->{type}){
-                        when ("string")     {$pushkey = $key ."=s";}
-                        when ("manystring") {$pushkey = $key ."=s@";}
-                        when ("keyvalue")   {$pushkey = $key ."=s%";}
-                        default             {$pushkey = $key; }
-                }
-                push @opt_spec, [$pushkey, $options->{$key}->{text}];
-        }
-        return (
-                @opt_spec
-               );
-}
 
 
 sub usage_desc
 {
-        my $allowed_opts = join ' ', map { '--'.$_ } _allowed_opts();
-        "tapper-testrun newscenario --file=s";
+        return abstract();
 }
 
-sub _allowed_opts
-{
-        my @allowed_opts = map { $_->[0] } opt_spec();
-}
 
 sub validate_args
 {
-        my ($self, $opt, $args) = @_;
-
-        my $msg = "Unknown option";
-        $msg   .= ($args and $#{$args} >=1) ? 's' : '';
-        $msg   .= ": ";
-        if (($args and @$args)) {
-                say STDERR $msg, join(', ',@$args);
-                die $self->usage->text;
-        }
-
-        die "Scenario file needed\n",$self->usage->text if not $opt->{file};
-        die "Scenario file ",$opt->{file}," does not exist" if not -e $opt->{file};
-        die "Scenario file ",$opt->{file}," is not readable" if not -r $opt->{file};
-
-        return 1;
+        die abstract,"\n";
 }
-
-=head2 execute
-
-Worker function
-
-=cut
-
-sub execute
-{
-        my ($self, $opt, $args) = @_;
-
-        my $scenario = do {local $/;
-                           open (my $fh, '<', $opt->{file}) or die "Can open file:$!\n";
-                           <$fh>
-                   };
-        $scenario = $self->apply_macro($scenario, $opt->{d});
-
-        my $scenario_conf = YAML::XS::Load($scenario);
-        given ($scenario_conf->{scenario_type}) {
-                when ('interdep') {
-                        $self->parse_interdep($scenario_conf, $opt);
-                }
-                default {
-                        die "Unknown scenario type ", $scenario_conf->{scenario_type};
-                }
-        };
-
-        return 0;
-}
-
-=head2 apply_macro
-
-Process macros and substit using Template::Toolkit.
-
-@param hashref - hash containing options
-@param hashref - hash containing arguments
-
-@return success - yaml text with applied macros
-@return error   - die with error string
-
-=cut
-
-sub apply_macro
-{
-        my ($self, $macro, $substitutes) = @_;
-
-        use Template;
-        my $tt            = new Template();
-        my $ttapplied;
-
-        $tt->process(\$macro, $substitutes, \$ttapplied) || die $tt->error();
-        return $ttapplied;
-}
-
-
-sub add_host
-{
-        my ($self, $testrun_id, $host) = @_;
-        my $cmd =  Tapper::Cmd::Requested->new();
-        my $id = $cmd->add_host($testrun_id, $host);
-        return $id;
-}
-
-
-sub add_feature
-{
-        my ($self, $testrun_id, $feature) = @_;
-        my $cmd = Tapper::Cmd::Requested->new();
-        my $id = $cmd->add_feature($testrun_id, $feature);
-        return $id;
-}
-
-sub analyse_preconditions
-{
-        my ($self, @ids) = @_;
-}
-
-
-=head2 parse_interdep
-
-Parse an interdep scenario and do everything needed to put it into the
-database.
-
-@param hash ref - config containing all relevant information
-@param hash ref - options
-
-@return success - 0
-@return error   - die with error text
-
-=cut
-
-sub parse_interdep
-{
-        my ($self, $conf, $opt) = @_;
-
-        my $scenario = Tapper::Cmd::Scenario->new();
-
-
-        # TODO:
-        # 1.) move into Tapper::Cmd
-        # 2.) make atomic
-        # 3.) check whether more than 2 testruns are local and prevent sync in this case
-
-        my $sc_id    = $scenario->add({type => 'interdep',
-                                       options => $conf->{scenario_options} || $conf->{options},
-                                      name => $conf->{scenario_name} || $conf->{name},
-                                      });
-        my @testrun_ids;
-        foreach my $testrun (@{$conf->{description}}) {
-                my $tr = Tapper::Cmd::Testrun->new();
-                $testrun->{scenario_id} = $sc_id;
-                my $testrun_id = $tr->add($testrun);
-                push @testrun_ids, $testrun_id;
-                my $precondition = Tapper::Cmd::Precondition->new();
-                my @ids = $precondition->add($testrun->{preconditions});
-                my $retval = $precondition->assign_preconditions($testrun_id, @ids);
-        }
-
-        if ($opt->{quiet}) {
-                say $sc_id;
-        } else {
-                say "scenario $sc_id consists of testruns ",join ", ",@testrun_ids;
-                say Tapper::Config->subconfig->{base_url} // 'http://localhost/tapper', "/testruns/idlist/", join (",",@testrun_ids);
-        }
-
-}
-
-
-
-# perl -Ilib bin/tapper-testrun new --topic=Software --precondition=14  --owner=ss5
 
 1;
